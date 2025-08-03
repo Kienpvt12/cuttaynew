e1ac714820e7a3df305baf582a9ac9eaf31aaf3c
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-08-03 15:58:50 +0700

    Update cuttayv2.lua

--- cuttayv2.lua
+++ cuttayv2.lua
@@ -33,8 +33,8 @@ getgenv().gagConfig = {
     KEEP_PETS_WEIGHT = 10,
 
     -- EQUIP_PETS Priority (Left -> Right)
-    EQUIP_PETS = { ["Starfish"] = 5, ["Mochi Mouse"] = 1,  ["Kodama"] = 1, ["Capybara"] = 1},
-    USE_PETS_FOR_UPGRADE_SLOT = { "Starfish", "Kodama" },
+    EQUIP_PETS = { ["Starfish"] = 5, ["Mochi Mouse"] = 1,  ["Tanchozuru"] = 1, ["Capybara"] = 1},
+    USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
 
     BUY_GEAR_SHOP = { "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler" },
     USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", ["Trading Ticket"] = 2 },
@@ -48,6 +48,7 @@ getgenv().gagConfig = {
     SHOW_WEBHOOK_USERNAME = true,
 } 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6cb882027ef5de19462b160764dcfb53.lua"))()
+
 
 
 
