c35898e843f965e1ba18b26b2ac5acc1a54cf5e7
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-08-17 04:09:03 +0700

    Update ayyaM1-2.lua

--- ayyaM1-2.lua
+++ ayyaM1-2.lua
@@ -36,7 +36,7 @@ getgenv().gagConfig = {
     KEEP_PETS_WEIGHT = 5,
 
     -- EQUIP_PETS Priority (Left -> Right)
-    EQUIP_PETS = { ["Sunny-Side Chicken"] = 5, [""Starfish""] = 3 },
+    EQUIP_PETS = { ["Sunny-Side Chicken"] = 5, ["Starfish"] = 3 },
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
 
     BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler", "Trading Ticket" },
@@ -52,3 +52,4 @@ getgenv().gagConfig = {
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/18f68a241a8acc1191c28d9a76d67fe3.lua"))()
+
