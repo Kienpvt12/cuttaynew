5e89ee74b1d8cb700e87241b468008802de07618
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-10-05 06:05:15 +0700

    Update ayyaM1-2.lua

--- ayyaM1-2.lua
+++ ayyaM1-2.lua
@@ -196,7 +196,7 @@ getgenv().ConfigsKaitun = {
 			},
 			["Limit Upgrade"] = 5, -- max is 5 (more than or lower than 1 will do nothing)
 			["Equip When Done"] = {
-						["Chubby Chipmunk"] = {3, 100, 1},
+						["Chubby Chipmunk"] = {8, 100, 1},
                 		["Sunny-Side Chicken"] = { 8, 100, 2 },
                 		["Rooster"] = { 5, 100, 6 },
 			},
@@ -298,5 +298,6 @@ getgenv().ConfigsKaitun = {
 }
 License = "dJSwsjHryaty4NoGBypJDqY9tQj0z3Fj"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
+
 
 
