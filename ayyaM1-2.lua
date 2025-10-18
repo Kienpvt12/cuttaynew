7520b8eb204804fd7bfc207e7dec6d39068f94b5
Kienpvt12 <153700448+Kienpvt12@users.noreply.github.com>
2025-10-18 23:37:01 +0700

    Update ayyaM1-2.lua

--- ayyaM1-2.lua
+++ ayyaM1-2.lua
@@ -54,8 +54,4 @@ getgenv().gagConfig = {
     GIFT_USERNAME = {},
     GIFT_PET = {},
 }
-
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c916e5b90dc37c71ecf1ec00dfce3d5d.lua"))()
-
-
-
